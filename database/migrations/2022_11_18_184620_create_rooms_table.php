1bdf80ba703da447e9aac43a3656737a2debddb8
Dream-Dev-io <pairprogramming4u@gmail.com>
2022-11-20 20:16:14 -0800

    make it bester

---
+++ database/migrations/2022_11_18_184620_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rooms_no');
+            $table->string('type');
+            $table->text('description');
+            $table->integer('price');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
